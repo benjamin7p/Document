940dbdba1dc4eeb858704b8a8fa2308043e06a41
Benjamin PRO <benjaminpoulsenpro@benjamins-macbook-air.local>
2018-09-14 18:06:42 -0600

    completed collections lab

--- 2 - Introduction to UIKit/5 - Collections/lab/Lab - Collections.playground/Pages/3. Exercise - Dictionaries.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/5 - Collections/lab/Lab - Collections.playground/Pages/3. Exercise - Dictionaries.xcplaygroundpage/Contents.swift
@@ -12,16 +12,25 @@ print(months)
 /*:
  Using subscripting syntax to add April to the collection with a value of 30. Print the dictionary.
  */
-var months = ["April", 30]()
+
+months["Apr"] = 30
+print(months)
+
 
 /*:
  It's a leap year! Update the number of days in February to 29 using the `updateValue(_:, forKey:)` method. Print the dictionary.
  */
+months.updateValue(29, forKey: "Feb")
+print(months)
 
 
 /*:
  Use if-let syntax to retrieve the number of days under "January". If the value is there, print "January has 31 days", where 31 is the value retrieved from the dictionary.
  */
+if let oldValue = months.updateValue(31, forKey: "Jan" ) {
+    print("January has 31 days")
+}
+
 
 
 /*:
@@ -29,11 +38,16 @@ var months = ["April", 30]()
  */
 let shapesArray = ["Circle", "Square", "Triangle"]
 let colorsArray = ["Red", "Green", "Blue"]
-
+let colors: [String: [String]] = [
+    "shapes" : shapesArray,
+    "colors" : colorsArray
+]
+print(colors)
 
 /*:
  Print the last element of `colorsArray`, accessing it through the dictionary you've created. You'll have to use if-let syntax or the bang operator to unwrap what is returned from the dictionary before you can access element of the array.
  */
+print(colorsArray[2])
 
 
 //: [Previous](@previous)  |  page 3 of 4  |  [Next: App Exercise - Pacing](@next)
