6d38c24435bba1ccd4e3cbd79abf63c56c40c276
Benjamin PRO <benjaminpoulsenpro@benjamins-macbook-air.local>
2018-10-26 10:21:15 -0600

    protocol lab

--- 4 - Tables and Persistence/1 - Protocols/lab/Lab - Protocols.playground/Pages/3. Exercise - Create a Protocol.xcplaygroundpage/Contents.swift
+++ 4 - Tables and Persistence/1 - Protocols/lab/Lab - Protocols.playground/Pages/3. Exercise - Create a Protocol.xcplaygroundpage/Contents.swift
@@ -6,25 +6,43 @@
 protocol Vehicle {
     var numberOfWheels: Int { get }
     
+    
     func drive()
 }
+
 
 /*:
  Define a `Car` struct that implements the `Vehicle` protocol. `numberOfWheels` should return a value of 4, and `drive()` should print "Vroom, vroom!" Create an instance of `Car`, print its number of wheels, then call `drive()`.
  */
 struct Car: Vehicle {
-    var numberOfWheels: Int
+    var numberOfWheels: Int {
+        return 4
+    }
     
-    
-}
     func drive() {
-        print("vroom, vroom")
-    
+        print("Vroom, Vroom")
+    }
 }
+let car1 = Car()
+car1.drive()
+
+
+
+
+
+
 
 /*:
  Define a `Bike` struct that implements the `Vehicle` protocol. `numberOfWheels` should return a value of 2, and `drive()` should print "Begin pedaling!". Create an instance of `Bike`, print its number of wheels, then call `drive()`.
  */
-
-
+struct Bike: Vehicle {
+    var numberOfWheels: Int {
+        return 2
+    }
+    func drive() {
+        print("begin pedaling")
+    }
+}
+let bike = Bike()
+bike.drive()
 //: [Previous](@previous)  |  page 3 of 5  |  [Next: App Exercise - Similar Workouts](@next)
