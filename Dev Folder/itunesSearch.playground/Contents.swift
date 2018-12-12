583dae13e1ff823c421e303663833fdae51ec165
Benjamin PRO <benjaminpoulsenpro@benjamins-macbook-air.local>
2018-12-12 09:38:06 -0700

    iTunes search lab done

--- Dev Folder/itunesSearch.playground/Contents.swift
+++ Dev Folder/itunesSearch.playground/Contents.swift
@@ -38,10 +38,10 @@ let url = baseUrl?.withQueries(query)!
 let url2 = baseUrl?.withQueries(query2)
 
 
-let task = URLSession.shared.dataTask(with: url2!) { (data, response, error) in
-if let data = data,
-    let string = String(data: data, encoding: .utf8) {
-    print(string)
+let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+    if let data = data,
+        let string = String(data: data, encoding: .utf8) {
+        print(string)
     }
 }
 task.resume()
@@ -49,9 +49,68 @@ task.resume()
 PlaygroundPage.current.finishExecution()
 
 
-struct StoreItem {
-    var name: String
-    var album: String
-    var track: String
+struct StoreItem: Codable {
+    var artist: String
+    var title: String
+    var description: String
     var url: URL
+    var longDescription: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case artist
+        case title
+        case description
+        case url
+        case longDescription
+    }
+    
+    init(from decoder: Decoder) throws {
+        let valueContainer = try decoder.container(keyedBy: CodingKeys.self)
+        artist = try valueContainer.decode(String.self, forKey: CodingKeys.artist)
+        title = try valueContainer.decode(String.self, forKey: CodingKeys.title)
+        description = try valueContainer.decode(String.self, forKey: CodingKeys.description)
+        url = try valueContainer.decode(URL.self, forKey: CodingKeys.url)
+        
+        if let longDescription = try? valueContainer.decode(String.self, forKey: CodingKeys.longDescription)  {
+        self.longDescription = longDescription
+        } else {
+            let additionalValues = try decoder.container(keyedBy: AdditionalKeys.self)
+            longDescription = (try? additionalValues.decode(String.self, forKey: AdditionalKeys.longDescription)) ?? ""
+        }
+    }
+    enum AdditionalKeys: String, CodingKey {
+        case longDescription
+    }
 }
+
+
+func fetchItems(matching query: [String: String], completion: @escaping ([StoreItem]?) -> Void) {
+    
+    let baseUrl = URL(string: "https://itunes.apple.com/search")
+    
+    guard let url = baseUrl?.withQueries(query) else {
+        completion(nil)
+        print("unable to build URL with supplied queries")
+        return
+    }
+    
+    
+    let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+        let decoder = JSONDecoder()
+        if let data = data,
+            let storedItems = try?
+                decoder.decode(StoreItems.self, from: data) {
+            completion(storedItems.results)
+        } else {
+            print("Either no data was returned, or data was not serialized")
+            completion(nil)
+            return
+        }
+    }
+    task.resume()
+}
+struct StoreItems: Codable {
+    let results: [StoreItem]
+}
+
+
